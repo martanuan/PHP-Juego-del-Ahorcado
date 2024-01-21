705d14c939f973fa8a7892ff230b4bff1296d26e
martanuan <martanyp@hotmail.com>
2024-01-21 12:55:40 +0100

    Rename file

---
+++ ahorcadoClass.php
@@ -0,0 +1,73 @@
+<?php
+
+session_start();
+class Ahorcado{
+
+    private $palabraSecreta = '';
+    private $intentos = 0;
+    private $letrasAdivinadas = [];
+    private $palabrasDisponibles = [];
+
+    public function __construct($palabrasDisponibles){
+        $this->palabrasDisponibles = $palabrasDisponibles;
+        $this->iniciarJuego();
+    }
+
+    private function iniciarJuego(){
+        $palabraAleatoria = $this->palabrasDisponibles[array_rand($this->palabrasDisponibles)];
+        $this->palabraSecreta = strtolower($palabraAleatoria);
+        $this->intentos = 0;
+        $this->letrasAdivinadas = array_fill_keys(str_split($this->palabraSecreta), false);
+    }
+
+    public function getPalabraSecreta(){
+        return $this->palabraSecreta;
+    }
+
+    public function getPalabraEnCurso(){
+        $palabraAdivinada = '';
+        foreach (str_split($this->palabraSecreta) as $letra){
+            if ($this->letrasAdivinadas[$letra]){
+                $palabraAdivinada .= $letra;
+            }else {
+                $palabraAdivinada .= '_ ';
+            }
+        }
+        return $palabraAdivinada;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
